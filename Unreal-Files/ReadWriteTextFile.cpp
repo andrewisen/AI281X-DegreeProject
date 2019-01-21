47abdb47831d99c743f1c5b134789b65f7227f04
Degree Project <andre.wisen@gmail.com>
2019-01-21 15:22:06 +0100

    Add temp bools

--- Unreal-Files/ReadWriteTextFile.cpp
+++ Unreal-Files/ReadWriteTextFile.cpp
@@ -2,3 +2,12 @@
 
 #include "ReadWriteTextFile.h"
 
+bool URWTxtFile::LoadTxt(FString FileNameA, FString& SaveTextA)
+{
+	return FFileHelper::LoadFileToString(SaveTextA, *(FPaths::GameDir() + FileNameA));
+}
+
+bool URWTxtFile::SaveTxt(FString SaveTextB, FString FileNameB)
+{
+	return FFileHelper::SaveStringToFile(SaveTextB, *(FPaths::GameDir() + FileNameB));
+}
