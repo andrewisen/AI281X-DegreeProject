7814fe0fd7ab92fbb030ab4897bffeb4beb0f88d
Degree Project <andre.wisen@gmail.com>
2019-01-22 11:25:57 +0100

    Add comments

--- Unreal-Files/ReadWriteTextFile.cpp
+++ Unreal-Files/ReadWriteTextFile.cpp
@@ -1,21 +1,26 @@
-// EU4 Test Class
+/**
+* Credit: https://answers.unrealengine.com/questions/120392/how-can-i-load-text-from-file-with-blueprint.html
+* 
+* See UE4s docs for more infromation about FFileHelper
+* https://api.unrealengine.com/INT/API/Runtime/Core/Misc/FFileHelper/SaveStringToFile/index.
+*/
 
 #include "ReadWriteTextFile.h"
 
+// Get value (string) from text file (relative path)
 bool URWTxtFile::LoadTxt(FString FileNameA, FString& SaveTextA)
 {
 	return FFileHelper::LoadFileToString(SaveTextA, *(FPaths::GameDir() + FileNameA));
 }
 
+// Set value (string) to text file (relative path)
 bool URWTxtFile::SaveTxt(FString SaveTextB, FString FileNameB)
 {
 	return FFileHelper::SaveStringToFile(SaveTextB, *(FPaths::GameDir() + FileNameB));
 }
 
-
+// Set value (string) to text file (absolute path)
 bool URWTxtFile::WriteTxt(FString SaveTextB, FString FileNameB)
 {
-	// https://api.unrealengine.com/INT/API/Runtime/Core/Misc/FFileHelper/SaveStringToFile/index.html
 	return FFileHelper::SaveStringToFile(SaveTextB, *(FileNameB));
 }
-
--- Unreal-Files/ReadWriteTextFile.h
+++ Unreal-Files/ReadWriteTextFile.h
@@ -1,4 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/**
+* Credit: https://answers.unrealengine.com/questions/120392/how-can-i-load-text-from-file-with-blueprint.html
+*
+* See UE4s docs for more infromation about FFileHelper
+* https://api.unrealengine.com/INT/API/Runtime/Core/Misc/FFileHelper/SaveStringToFile/index.html
+*/
 
 #pragma once
 
@@ -6,21 +11,22 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "ReadWriteTextFile.generated.h"
 
-/**
- * 
- */
+// Unreal Engine 4
 UCLASS()
 class LIGHTTEST_190117_API URWTxtFile : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY() public:
 
+		// Get value (string) from text file (relative path)
 		UFUNCTION(BlueprintPure, Category = "Custom", meta = (Keywords = "LoadTxt"))
 			static bool LoadTxt(FString FileNameA, FString& SaveTextA);
 
-		// N.B: A BlueprintCallable function will have execution pins so that you can chain your C++ functions together in UE4 Blueprints.
+		// Set value (string) to text file (relative path)
+		// A BlueprintCallable function will have execution pins so that you can chain your C++ functions together in UE4 Blueprints.
 		UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "SaveTxt"))
 			static bool SaveTxt(FString SaveTextB, FString FileNameB);
 
+		// Set value (string) to text file (absolute path)
 		UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "WriteTxt"))
 			static bool WriteTxt(FString SaveTextB, FString FileNameB);
 
