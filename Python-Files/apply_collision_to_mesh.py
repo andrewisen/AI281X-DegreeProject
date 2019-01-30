6b199ff7f386098ae0109dc4c1209c960cabae15
Degree Project <andre.wisen@gmail.com>
2019-01-30 17:51:07 +0100

    Add path variables

--- Python-Files/apply_collision_to_mesh.py
+++ Python-Files/apply_collision_to_mesh.py
@@ -2,6 +2,20 @@ import unreal
 import csv
 
 def main():
+	''' SET PATHS '''
+
+	### Unreal Assets Path
+	# N.b. Content should be replaced with "Game".
+	# Defualt: asset_path = "/Game/Revit/"
+	assetPath = "/Game/Revit/190130_RevitCollisionScene_01/"
+
+	### CSV Path
+	# Absolute Path 
+	csvAbsPath = "C:\\_DegreeProject-Spring19-AndreWisen\\190130 - Revit Collision\\ids.csv"
+	# Relative Path to 
+	csvRelPath = ""
+
+
 	print("main")
 
 if __name__ == '__main__':
