adb4e6405cdc879a02b49b94e38f64cd2b551ea8
Degree Project <andre.wisen@gmail.com>
2019-02-04 14:38:47 +0100

    Minor changes

--- Python-Files/apply_collision_to_mesh.py
+++ Python-Files/apply_collision_to_mesh.py
@@ -10,12 +10,12 @@ def GetCSV(csvPath):
 	return csvList
 
 def LoadAssets(assetPath):
-	# Based on: 
+	# Based on:
 	# https://docs.unrealengine.com/en-us/Editor/Scripting-and-Automating-the-Editor/Editor-Scripting-How-Tos/Setting-up-Collision-Properties-in-Blueprints-and-Python
 
-	# Get a list of all Assets in the path.
+	# Get a list of all Assets in the path
 	listOfAssets = unreal.EditorAssetLibrary.list_assets(assetPath)
-	# Load them ll into memory.
+	# Load them into memory
 	assets = [unreal.EditorAssetLibrary.load_asset(eachAsset) for eachAsset in listOfAssets]
 
 	return assets
@@ -38,38 +38,38 @@ def FilterAssets(csvList,assets):
 	return filteredAssets
 
 def AddBoxCollision(staticMesh):
-	# N.B You could instead use: 
-	#.SPHERE, .CAPSULE, N.DOP10_X, .NDOP10_Y, .NDOP10_Z, .NDOP18, .NDOP26
+	# N.B. You could instead use: 
+	# .SPHERE, .CAPSULE, N.DOP10_X, .NDOP10_Y, .NDOP10_Z, .NDOP18, .NDOP26
 	shapeType = unreal.ScriptingCollisionShapeType.BOX
 	
 	unreal.EditorStaticMeshLibrary.add_simple_collisions(staticMesh, shapeType)
 	unreal.EditorAssetLibrary.save_loaded_asset(staticMesh)
 
 def main():
-	''' SET PATHS '''
+	### SET PATHS ###
 
 	### Unreal Assets Path
-	# N.b. Content should be replaced with "Game".
+	# N.B. The "Content" folder should be replaced with "Game".
+	# Look at the info box when importing Datasmith Model.
 	# Defualt: asset_path = "/Game/Revit/"
 	assetPath = "/Game/Revit/"
 
 	### CSV Path
 	# Absolute Path 
 	csvAbsPath = "C:\\_DegreeProject-Spring19-AndreWisen\\190130 - Revit Collision\\ids.csv"
-	# Relative Path to 
+	# Relative Path  
 	csvRelPath = ""
 
-	''' GET DATA '''
-	# Get refrence items
+	### GET DATA ###
+	# Get refrence items. Choose absolute or relative path as parameter
 	csvList = GetCSV(csvAbsPath)
 	# Get all items
 	assets = LoadAssets(assetPath)
 
-
-	''' FILTER DATA '''
+	### FILTER DATA ###
 	filteredAssets = FilterAssets(csvList,assets)
 	
-	''' ADD COLLISION '''
+	### ADD COLLISION ###
 	for eachAsset in filteredAssets:
 		map(AddBoxCollision, eachAsset)
 
