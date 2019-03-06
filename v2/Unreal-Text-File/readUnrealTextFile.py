f5068458f64419c5cbd09a1839efdbeaf7e9cf99
André Wisén <andre.wisen@gmail.com>
2019-03-06 12:26:24 +0100

    Add method for reading file

--- v2/Unreal-Text-File/readUnrealTextFile.py
+++ v2/Unreal-Text-File/readUnrealTextFile.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def readLines():
+	fileName = "data.t3d"
+	
+	try:
+		f = open(fileName,encoding='utf-16-le')
+	except:
+		return
+
+	lines = f.read().splitlines() # Without n/
+	#lines = f.readlines() # With n/
+
+	f.close()
+
+	return lines
+
 def main():
-  print("Hello World!")
-  
+	lines = readLines()
+	
+	for i in lines:
+		print(i)
+
 if __name__== "__main__":
-  main()
+	main()
