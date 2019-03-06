87175fc15341635ef240fbc886bb2a0c9cad1745
André Wisén <andre.wisen@gmail.com>
2019-03-06 13:01:03 +0100

    Add function for getting objects

--- v2/Unreal-Text-File/readUnrealTextFile.py
+++ v2/Unreal-Text-File/readUnrealTextFile.py
@@ -2,25 +2,51 @@
 # -*- coding: utf-8 -*-
 
 def readLines():
-	fileName = "data.t3d"
+	# NB. Be aware of realtive or absolute path
+	file = "data.t3d" 
 	
 	try:
-		f = open(fileName,encoding='utf-16-le')
+		fileObject = open(file,encoding='utf-16')
 	except:
+		print("Error: File not loaded")
 		return
 
-	lines = f.read().splitlines() # Without n/
-	#lines = f.readlines() # With n/
-
-	f.close()
+	lines = fileObject.read().splitlines()
+	fileObject.close()
 
 	return lines
 
+def getObjects(lines):
+	beginObjectPhrase = "Begin Object"
+	endObjectPhrase = "End Object"
+
+	status = ""
+	objects = []
+	currentObject = []
+
+	for line in lines:
+		if beginObjectPhrase in line: 
+			status = "begin"
+			pass
+		elif endObjectPhrase in line: 
+			status = "end"
+			objects.append(currentObject)
+			currentObject = []
+			pass
+		else:
+			if status == "end":
+				continue
+			else:
+				pass
+		
+		currentObject.append(line)
+
+	for i in objects:
+		print(i)
+
 def main():
 	lines = readLines()
-	
-	for i in lines:
-		print(i)
+	getObjects(lines)
 
 if __name__== "__main__":
 	main()
