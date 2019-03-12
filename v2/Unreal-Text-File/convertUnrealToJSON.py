53bcbdeda518a65eb338ebe2cdc1844afd4365f8
André Wisén <andre.wisen@gmail.com>
2019-03-12 15:50:51 +0100

    Add Python script that converts Unreal Text File to JSON tree

--- v2/Unreal-Text-File/convertUnrealToJSON.py
+++ v2/Unreal-Text-File/convertUnrealToJSON.py
@@ -1,2 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+from tabtree import parser
+
+def readLines():
+	file = "data.t3d"
+	
+	try:
+		fileObject = open(file,encoding='utf-16')
+	except Exception as e:
+		print("Error: File not loaded")
+		return
+
+	lines = fileObject.read().splitlines()
+	#lines = fileObject.read()
+	
+	fileObject.close()
+
+	return lines
+
+def convert(lines):
+	beginString = "Begin "
+	endString = "End "
+
+	print(lines)
+		
+
+def main():
+	lines = readLines()
+	convert(lines)
 if __name__== "__main__":
 	main()
