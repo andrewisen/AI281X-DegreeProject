5720cea4607a63044c0252c82c700ac0d30aac1d
André Wisén <andre.wisen@gmail.com>
2019-03-19 10:56:36 +0100

    Minor changes

--- v2/Unreal-JSON/unrealJSON.py
+++ v2/Unreal-JSON/unrealJSON.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import ast
+import sys
 
 def readLines():
 	file = "data.t3d"
@@ -19,15 +20,51 @@ def readLines():
 		#print("Error: File not loaded")
 		pass
 
-
 	lines = fileObject.read()
-	splitlines = fileObject.read().splitlines()
+	splitlines = lines.splitlines()
 	fileObject.close()
 
 	return lines,splitlines
 
+def replaceDoubleQuoutes(lines):
+	array = []
+	for line in lines:
+		#line = line.replace('"','\"')
+		line = line.replace('"','---')
+		array.append(line)
+	return array
+
+def addSeperator(lines):
+	searchStringWithSpace = "Begin "
+	searchString = "Begin"
+
+	for line in lines:
+		if searchStringWithSpace not in line:
+			continue
+
+		words = line.split(" ") 
+		
+
+		searchStringIndex = words.index(searchString)
+		seperatorWord = words[searchStringIndex]
+		seperatorWord = '"' + seperatorWord
+		words[searchStringIndex] = seperatorWord
+
+		words[-1] = words[-1] + '"'
+
+		print(words)
+
+
 def main():
 	lines, splitlines = readLines()
+	# Lines is a string
+	# Splitlines is a list containing each row
+
+	lines = splitlines
+
+	lines = replaceDoubleQuoutes(lines)
+	addSeperator(lines)
+
 
 if __name__== "__main__":
 	main()
