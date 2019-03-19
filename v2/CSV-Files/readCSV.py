9389777c83d70f5b03c6430f5faa3658d8a76f82
André Wisén <andre.wisen@gmail.com>
2019-03-19 15:33:47 +0100

    Add function that reads CSV

--- v2/CSV-Files/readCSV.py
+++ v2/CSV-Files/readCSV.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import csv
+def readCSV():
+	fileBefore = "CSV_FileA.csv"
+	fileAfter = "CSV_FileB.csv"
+	delimiter = ";"
+	try:
+		csvBefore = open(fileBefore)
+	except Exception as e:
+		print("Error: File (before) not loaded")
+		return
+	try:
+		csvAfter = open(fileAfter)
+	except Exception as e:
+		print("Error: File (after) not loaded")
+		return
+
+	csvBefore = csv.reader(csvBefore, delimiter=delimiter)
+	csvAfter = csv.reader(csvAfter, delimiter=delimiter)
+	
+	return csvBefore,csvAfter
+
 
 def main():
-	print("Foobar")
+	csvBefore,csvAfter = readCSV()
+	for i in csvBefore:
+		print(i)
 
 if __name__== "__main__":
 	main()
