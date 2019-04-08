ed6ede1df1fa266e6cd4c19bdde9162708455426
André Wisén <andre.wisen@gmail.com>
2019-04-08 17:36:57 +0200

    Minor changes

--- v2/Docker-Files/myPythonFile.py
+++ v2/Docker-Files/myPythonFile.py
@@ -1,1 +1,15 @@
-print("Hello Docker")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def openFile():
+	file = open("text.txt", "w")
+
+	file.write("New line") 
+
+	file.close()
+
+def main():
+	openFile()
+
+if __name__== "__main__":
+	main()
+
